3c66e4dea81d71f24f5b75dd0f41608fa43361e8
Ioana Lazar <ioanalazar2008@yahoo.com>
2020-05-01 21:58:47 +0300

    First simple login screen

---
+++ src/sample/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Controller {
+
+    public void loginClicked(){
+        System.out.println("Login code");
+    }
+
+    public void registerClicked(){
+        System.out.println("Register code");
+    }
+}
+
+
