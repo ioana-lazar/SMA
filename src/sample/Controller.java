f724345f2fa80b92cccb5505d79b24ef99706379
Andrei Micu <andreimihai394@hotmail.com>
2020-05-03 18:46:19 +0300

    Password encoding, PopUps

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,12 +5,13 @@ import javafx.scene.control.TextField;
 public class Controller {
     public TextField namefield, passwordfield;
     Database database = new Database();
+    PopUp popUp;
 
     public void loginClicked(){
         User user = new User(namefield.getText(), passwordfield.getText());
         if(!database.contains(user))
         {
-            System.out.println("User or password incorrect");                           //pop up cu mesaj
+            popUp.display("Login failed", "User or password incorrect");
             namefield.clear();
             passwordfield.clear();
         }
@@ -23,10 +24,11 @@ public class Controller {
 
     public void registerClicked()
     {
-        User user = new User(namefield.getText(), passwordfield.getText());
+        User user = new User(namefield.getText(),  database.encodePassword(namefield.getText(), passwordfield.getText()));
+
         if(database.containsUsername(user.getName()))
         {
-            System.out.println("Username already exists");                           //pop up cu mesaj
+            popUp.display("Registration failed", "Username already exists");
             namefield.clear();
             passwordfield.clear();
         }
@@ -35,11 +37,12 @@ public class Controller {
             if(user.checkUsername(user.getName()) && user.checkPassword(user.getPassword()))
             {
                 database.addUser(user);
-                System.out.println("Registered!");
+                popUp.display("Registration succeeded", "User " + user.getName() + " is now registered!");
             }
             else
             {
-                System.out.println("Username must be between 5 - 15 characters(only letters and numbers allowed).\nPassword must be between 7 - 12 characters\n");
+                popUp.display("Warning", "Username must be between 5 - 15 characters(only letters and numbers allowed).\nPassword must be between 7 - 12 characters");
+
             }
 
         }
--- src/sample/PopUp.java
+++ src/sample/PopUp.java
@@ -1,4 +1,32 @@
 package sample;
 
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
 public class PopUp {
+    public static void display(String title, String message){
+        Stage window = new Stage();
+        window.setTitle(title);
+        window.setMinWidth(300);
+
+        Label label = new Label();
+        label.setText(message);
+        label.setFont(Font.font(16));
+        Button button = new Button();
+        button.setText("OK");
+        button.setOnAction(e -> window.close());
+
+        VBox layout = new VBox(20);
+        layout.getChildren().addAll(label, button);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout, 300, 200);
+        window.setScene(scene);
+        window.showAndWait();
+    }
 }
